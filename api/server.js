7c05d948ff57d3a1812cd63654b9d111a7d9f210
Bryxnt1 <bryantperez56@gmail.com>
2023-08-23 21:16:32 -0400

    work

--- api/server.js
+++ api/server.js
@@ -5,10 +5,13 @@ const cors = require('cors')
 const app = express();
 
 app.use(express.json());
-app.use('cors'());
+app.use(cors());
 
 mongoose.connect("mongodb://127.0.0.1:27017/To-Do", {
-    useNewUrlParser: SVGComponentTransferFunctionElement,
+    useNewUrlParser: true,
     useUnifiedTopology: true
 
-}).then(() => console.log("Connected to Database") )
+}).then(() => console.log("Connected to Database"))
+.catch(console.eror);
+
+app.listen(3001,() => console.log("server started on port 3001 LETS GOO"));
