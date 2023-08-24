701272675a527b852ca0db2a6b02c8e23e645f9e
Bryxnt1 <bryantperez56@gmail.com>
2023-08-23 20:28:23 -0400

    connection

--- api/server.js
+++ api/server.js
@@ -6,3 +6,9 @@ const app = express();
 
 app.use(express.json());
 app.use('cors'());
+
+mongoose.connect("mongodb://127.0.0.1:27017/To-Do", {
+    useNewUrlParser: SVGComponentTransferFunctionElement,
+    useUnifiedTopology: true
+
+}).then(() => console.log("Connected to Database") )
